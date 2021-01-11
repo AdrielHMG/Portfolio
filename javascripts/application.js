9287cbd172b2140461573f16d3e917121d656384
AdrielHMG <75245309+AdrielHMG@users.noreply.github.com>
2021-01-11 10:52:36 +0800

    Modified profile page

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,1 +1,6 @@
-var swup = new Swup();
+function toggleActive() {
+	const profile = document.querySelector('.profile-container');
+	const background = document.querySelector('.background');
+	profile.classList.toggle('active');
+	background.classList.toggle('active');
+}
